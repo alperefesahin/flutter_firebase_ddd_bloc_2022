804646b13afc51e5a48c293d0213ad20a584ce04
sahinefe35 <efe.mate@gmail.com>
2022-10-18 16:03:44 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_firebase_ddd_riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_firebase_ddd_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const AppWidget());
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maconst terial App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
