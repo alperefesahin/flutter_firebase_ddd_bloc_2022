63ae6b6d4665faae9c59a7594b3f9246e825aca3
sahinefe35 <efe.mate@gmail.com>
2022-10-20 19:07:24 +0300

    project name problems & firebase implementation

--- lib/application/auth/sign_in_form/sign_in_form_state.dart
+++ lib/application/auth/sign_in_form/sign_in_form_state.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_firebase_ddd_bloc_2022/domain/auth/auth_failure.dart';
-import 'package:flutter_firebase_ddd_bloc_2022/domain/auth/value_objects.dart';
+import 'package:flutter_firebase_ddd_riverpod/domain/auth/auth_failure.dart';
+import 'package:flutter_firebase_ddd_riverpod/domain/auth/value_objects.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'sign_in_form_state.freezed.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_ddd_riverpod/firebase_options.dart';
 
-void main() => runApp(const AppWidget());
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const AppWidget());
+}
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
