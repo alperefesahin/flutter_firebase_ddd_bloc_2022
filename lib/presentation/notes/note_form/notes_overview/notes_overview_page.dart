00c09582cf3bd008bb48dab145c069839b5db6bd
sahinefe35 <efe.mate@gmail.com>
2022-10-25 21:38:55 +0300

    Flutter Firebase & DDD Course [19] done

---
+++ lib/presentation/notes/note_form/notes_overview/notes_overview_page.dart
@@ -0,0 +1,79 @@
+import 'package:another_flushbar/flushbar_helper.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_ddd_bloc/application/auth/auth_bloc.dart';
+import 'package:flutter_firebase_ddd_bloc/application/notes/note_actor/note_actor_bloc.dart';
+import 'package:flutter_firebase_ddd_bloc/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:flutter_firebase_ddd_bloc/injection.dart';
+import 'package:flutter_firebase_ddd_bloc/presentation/routes/router.gr.dart';
+
+class NotesOverviewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NoteWatcherBloc>(
+          create: (context) => getIt<NoteWatcherBloc>()
+            ..add(const NoteWatcherEvent.watchAllStarted()),
+        ),
+        BlocProvider<NoteActorBloc>(
+          create: (context) => getIt<NoteActorBloc>(),
+        ),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              state.maybeMap(
+                unauthenticated: (_) =>
+                    context.router.replace(const SignInRoute()),
+                orElse: () {},
+              );
+            },
+          ),
+          BlocListener<NoteActorBloc, NoteActorState>(
+            listener: (context, state) {
+              state.maybeMap(
+                deleteFailure: (state) {
+                  FlushbarHelper.createError(
+                    duration: const Duration(seconds: 5),
+                    message: state.noteFailure.map(
+                      unexpected: (_) =>
+                          'Unexpected error occured while deleting, please contact support.',
+                      insufficientPermission: (_) =>
+                          'Insufficient permissions ❌',
+                      unableToUpdate: (_) => 'Impossible error',
+                    ),
+                  ).show(context);
+                },
+                orElse: () {},
+              );
+            },
+          ),
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Notes'),
+            leading: IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                context.read<AuthBloc>().add(const AuthEvent.signedOut());
+              },
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.indeterminate_check_box),
+              )
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+  }
+}
