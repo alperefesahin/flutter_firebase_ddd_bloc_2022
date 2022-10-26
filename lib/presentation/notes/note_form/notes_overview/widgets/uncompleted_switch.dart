d13805f986aa3a85b8e1a8344d89a8bf18cff181
sahinefe35 <efe.mate@gmail.com>
2022-10-26 14:20:57 +0300

    Flutter Firebase & DDD Course [22] done

---
+++ lib/presentation/notes/note_form/notes_overview/widgets/uncompleted_switch.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_ddd_bloc/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class UncompletedSwitch extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final toggleState = useState(false);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: InkResponse(
+        onTap: () {
+          toggleState.value = !toggleState.value;
+          context.read<NoteWatcherBloc>().add(
+                toggleState.value
+                    ? const NoteWatcherEvent.watchUncompletedStarted()
+                    : const NoteWatcherEvent.watchAllStarted(),
+              );
+        },
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 100),
+          transitionBuilder: (child, animation) => ScaleTransition(
+            scale: animation,
+            child: child,
+          ),
+          child: toggleState.value
+              ? const Icon(
+                  Icons.check_box_outline_blank,
+                  key: Key('outline'),
+                )
+              : const Icon(
+                  Icons.indeterminate_check_box,
+                  key: Key('indeterminate'),
+                ),
+        ),
+      ),
+    );
+  }
+}
